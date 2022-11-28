56a9db606b0fddf7c07f0ae08999702533d6c8b9
DanielUnleashed <dbejarcaballero@gmail.com>
2022-11-28 11:44:18 +0100

    Correct bit monitoring and higher baudrate

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,15 @@
 #define ADDRESS_START 23
 #define DATA_START 24
 
+void printInBIN(uint8_t n){
+  for(uint8_t i = 0; i < 8; i++){
+    bool b = n>>(7-i);
+    Serial.print(b);
+  }
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   pinMode(CLOCK, INPUT);
   pinMode(RWB, INPUT);
@@ -31,21 +38,26 @@ void loop() {
       Serial.print(bit);
     }
   
-    Serial.print("\t");
+    printInBIN(address>>8);
+    printInBIN(address);
 
     uint8_t data = 0;
     for(uint8_t i = 0; i < 8; i++){
       uint8_t bit = digitalRead(DATA_START+i*2);
       data = (data << 1) | bit;
-      Serial.print(bit);
     }
+
+    Serial.print("\t");
+    printInBIN(data);
 
     char readOrWrite = digitalRead(RWB)?'r':'w';
 
     char output[50];
-    sprintf(output, "\tAdd: %04x\tData: %02x\t%c\n", address, data, readOrWrite);
+    sprintf(output, "\tAdd: %04X\tData: %02X\t%c\n", address, data, readOrWrite);
     Serial.print(output);
   }
   lastClockState = currentClockState;
 }
+
+
 
